85b5c779c875363dd8aa24dd2694e66bcefe4700
vaishnavi <shindevai2004@gmail.com>
2025-11-13 18:12:25 +0530

    .

--- AgriSens-web-app/js/main.js
+++ AgriSens-web-app/js/main.js
@@ -10,3 +10,16 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 };
+
+// number
+document.getElementById("callButton").addEventListener("click", function(e) {
+                e.preventDefault();
+                const phone = "18001801551";
+                if (/Mobi|Android/i.test(navigator.userAgent)) {
+                  // Mobile device
+                  window.location.href = "tel:" + phone;
+                } else {
+                  // Not a mobile device
+                  alert("Please use a mobile device to make this call.");
+                }
+              });
